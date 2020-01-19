7ed6741e461f1b51d993cc186b75305cef8de54b
1800147 <tychen742@gmail.com>
2020-01-19 13:08:44 -0500

    update

--- user/logout.php
+++ user/logout.php
@@ -8,12 +8,14 @@ include('../includes/includes.php');
 
     <!-- ########### clear session ############ -->
     <?php
+
     echo "<span class='php-message' > Logging " . $_SESSION['username_hbdi'] . " out..." . "</span>";
-    unset($_SESSION['username_hbdi']);
-    unset($_SESSION['email_hbdi']);
-    unset($_SESSION['id_user']);
-    //            unset($_SESSION['login_request']);  // no need because unset at login_process.php
-    echo '<meta http-equiv=REFRESH CONTENT=1;url=' . $p . '/index.php>';
+
+    session_destroy();
+
+    echo '<meta http-equiv=REFRESH CONTENT=1;url=' . $p . 'tychen/index.php>';
+
+    exit;
     ?>
 
 </div>

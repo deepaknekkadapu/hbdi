2a0b682540f9c8fecf3e599ba83aa946aa4f26a4
tychen <tychen742@gmail.com>
2020-01-22 11:26:53 -0500

    update

--- includes/includes.php
+++ includes/includes.php
@@ -2,10 +2,7 @@
 //include("headers.php");
 //include("footer.php");
 include_once("topnav.php");
-<<<<<<< HEAD
 include_once("../includes/login_loader.php");
-=======
 include_once("login_loader.php");
->>>>>>> 2df5ac8d4c069e95816d93b93dfe287ff7c104d3
 ?>
 
